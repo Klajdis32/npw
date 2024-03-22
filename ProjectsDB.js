e027b93e1b77974751912717a33a2ed454e638b1
klaintis <kcami32@outlook.com>
2024-03-22 20:33:28 +0200

    alages sta project

--- ProjectsDB.js
+++ ProjectsDB.js
@@ -6,7 +6,9 @@ const posts = [
         Date: "15/04/2023",
         Katigoria: "Physics", 
         Keimeno: "This project delves into the realm of physics, exploring the experimental representation of linear, smooth, and accelerated body motion. Our aim is to corroborate the mathematical equations that describe these motions through real-world experimentation. The process utilizes a unique setup - the layout of an air route, supported by a computer for data recording and analysis. The mathematical validity is ensured by employing the method of Ordinary Least Squares.",
-        Link: "/npw/attachments/pdfs/SimulationOfLinearMotion.pdf" 
+        Link: "" ,
+        pdf: "/npw/attachments/pdfs/SimulationOfLinearMotion.pdf",
+        file: ""
     },
     {
         image: "/npw/ProjectImages/arduino.jpg", 
@@ -15,6 +17,8 @@ const posts = [
         Date: "13/04/2023",
         Katigoria: "Electronics", 
         Keimeno: "In this project I had to connect a potentiometer to a servo and thus determine through it the angle that the servo will move without putting the angle in the c++ code. The led is connected to a 150 Ω resistor and simply when the angles I determine through the potentiometer are small, it flashes very quickly, while when the angle increases, its frequency decreases.",
-        Link: "https://www.tinkercad.com/things/kSZySV6dCZB?sharecode=ktuk_1gy83ruaVoYp3rV64uu0xQLY0x-EDiKvdFDQIs" 
+        Link: "https://www.tinkercad.com/things/kSZySV6dCZB?sharecode=ktuk_1gy83ruaVoYp3rV64uu0xQLY0x-EDiKvdFDQIs",
+        pdf: "",
+        file: ""
     },
 ];
