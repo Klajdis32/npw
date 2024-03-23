b5e00eefd87cf438a2b56dac52d2e8cdb547d0c6
klaintis <kcami32@outlook.com>
2024-03-23 10:55:00 +0200

    nea projects

--- ProjectsDB.js
+++ ProjectsDB.js
@@ -1,5 +1,125 @@
 const posts = [
     {
+        image: "/npw/ProjectImages/game.png", 
+        Titlos: "Rocket Game With Python", 
+        Sigrafeas: "klajdi", 
+        Date: "24/07/2023",
+        Katigoria: "Programming", 
+        Keimeno: "Rocket Game is an exciting space-themed game that puts players in control of a rocket traveling through the cosmos. The objective is to navigate the rocket through a challenging environment, avoiding obstacles and destroying enemies to score points and progress through different levels.",
+        Link: "https://github.com/Klajdis32/RocketGameWithPython.git" ,
+        pdf: "",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/skyline.png", 
+        Titlos: "Skyline Problem Algorithm With Java", 
+        Sigrafeas: "Kristi", 
+        Date: "17/07/2023",
+        Katigoria: "Programming", 
+        Keimeno: "In this project, we aimed to develop a code that calculates the skyline of a set of points. The skyline represents the upper boundaries of a region, where each point is visible and not covered by any other point. Overall, this java code implements an algorithm that efficiently computes the skyline of a set of points, ensuring that only the visible and non-overlapping points are included.",
+        Link: "https://github.com/Klajdis32/SkylineProblemAlgorithm" ,
+        pdf: "",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/MaPassImg.png", 
+        Titlos: "Android App Manage Passwords (MaPass)", 
+        Sigrafeas: "Kristi", 
+        Date: "13/07/2023",
+        Katigoria: "Programming", 
+        Keimeno: "In this project, our aim was to create a password management application for Android called MaPass. In today's digital world, the need for strong passwords is essential to protect our personal and sensitive information. However, managing these passwords can be quite challenging. MaPass is an innovative password management application that we have developed specifically for Android, combining security and convenience into one functional tool. With MaPass, you can securely store and organize all your passwords in one place, ensuring easy access whenever you need them.",
+        Link: "https://github.com/KristiCami/PasswordManager" ,
+        pdf: "/npw/attachments/pdfs/MaPass.pdf",
+        file: "",
+        video: "/npw/attachments/videos/video.mp4"
+    },
+    {
+        image: "/npw/ProjectImages/ear.jpg", 
+        Titlos: "Ultrasound", 
+        Sigrafeas: "klajdi", 
+        Date: "14/06/2023",
+        Katigoria: "Physics", 
+        Keimeno: "This project delves into the fundamental acoustic phenomena of wave propagation and reflection, aiming to determine the frequency and wavelength of sound waves through the use of an appropriate ultrasound setup. Through extensive experimentation, this study explores the behavior of sound waves and their interaction , with a specific focus on reflection phenomena. By designing and implementing a well-suited ultrasound arrangement, we conducted experiments to accurately measure and analyze the frequency and wavelength of these waves.",
+        Link: "" ,
+        pdf: "/npw/attachments/pdfs/Ultrasound.pdf",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/palmografos.jpeg", 
+        Titlos: "Oscilloscope", 
+        Sigrafeas: "klajdi", 
+        Date: "01/06/2023",
+        Katigoria: "Physics", 
+        Keimeno: "The proposed project consists of three distinct experiments aimed at precisely measuring the amplitude, frequency, and phase of sinusoidal signals. The equipment used to implement this project includes a dual-beam oscilloscope and a frequency generator. In simple terms, through the frequency generator, which we connect to the oscilloscope using a cable, we send various signals of specific or unknown frequency towards the oscilloscope. We study each of these signals via the screen contained in the oscilloscope, with the aim of obtaining experimental data. This experimental data, through mathematical analysis, will lead us to the unknown values of amplitude, frequency, and phase of the sinusoidal signals that we aim to measure in each experiment. This methodical approach provides a hands-on experience of the fundamental principles of signal processing and waveform analysis, offering significant insights into the nature of sinusoidal signals.",
+        Link: "" ,
+        pdf: "/npw/attachments/pdfs/oscilloscope.pdf",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/(ALU).png", 
+        Titlos: "Arithmetic logic unit (Alu)", 
+        Sigrafeas: "Kristi", 
+        Date: "16/05/2023",
+        Katigoria: "Electronics", 
+        Keimeno: "In this project, our objective was to design and implement an Arithmetic Logic Unit (ALU) using Logisim, a digital logic simulator. The ALU served as a crucial component responsible for performing arithmetic and logical operations. We started by designing the ALU's architecture, considering the necessary subunits such as adders, multipliers, dividers, and logic gates. Through careful interconnection of these subunits, we created a functional ALU. We conducted extensive simulations to test its accuracy and reliability, verifying that it produced correct results for various test cases. Throughout the project, we encountered challenges and developed problem-solving skills while gaining a deeper understanding of digital logic and circuit design. To further demonstrate our work, we have made the ALU design file available for review and testing. You can download and run it on Logisim to see our ALU in action.",
+        Link: "" ,
+        pdf: "",
+        file: "/npw/attachments/files/ALU.zip",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/thermometer1920.jpg", 
+        Titlos: "Specific heat of materials", 
+        Sigrafeas: "klajdi", 
+        Date: "13/05/2023",
+        Katigoria: "Physics", 
+        Keimeno: "This project explores the fascinating field of thermodynamics through a series of experiments, aiming to determine the heat capacities of various substances such as distilled water, glycerin, and lead. By combining practical experimentation with mathematical analysis, we are delving into the fundamental principles that govern the transfer of heat energy. Our experiments involve heating these substances under controlled conditions and carefully measuring the amount of heat absorbed. Through the application of mathematical concepts, we can then calculate their specific heat capacities. This project provides invaluable insights into the complex interplay of heat, energy, and matter, offering a hands-on understanding of the principles of thermodynamics.",
+        Link: "" ,
+        pdf: "/npw/attachments/pdfs/SpecificHeatOfMaterials.pdf",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/blue-ge.jpg", 
+        Titlos: "Rotated Cube", 
+        Sigrafeas: "klajdi", 
+        Date: "13/05/2023",
+        Katigoria: "Programming", 
+        Keimeno: "This project is about how we can display a rotating cube through the c programming language. The code of this project is uploaded to the github, the first code with a name SizeReduction refers only to the rotation of the cube around itself and the reduction of its size, while in the second one with name RotatedCube we also add the rotation.",
+        Link: "https://github.com/Klajdis32/RotatedCube" ,
+        pdf: "",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/ohmmeter-1920.jpg", 
+        Titlos: "Useful electrical circuits", 
+        Sigrafeas: "klajdi", 
+        Date: "07/05/2023",
+        Katigoria: "Electronics", 
+        Keimeno: "The electrical circuits presented in this project are very useful, specifically the internal resistance of an ammeter and a voltmeter is measured, a procedure that can be followed by anyone who wants to measure the internal resistance of the ammeter or voltmeter used in their circuit. Furthermore, through a new circuit, we extend the measurement range of an ammeter, while at the end we show how an unknown resistance can be measured.",
+        Link: "" ,
+        pdf: "/npw/attachments/pdfs/UsefulElectricalCircuits.pdf",
+        file: "",
+        video: ""
+    },
+    {
+        image: "/npw/ProjectImages/Fractal.png", 
+        Titlos: "Pentagonal Fractal With C", 
+        Sigrafeas: "klajdi", 
+        Date: "06/05/2023",
+        Katigoria: "Programming", 
+        Keimeno: "In this project, through the c programming language, we display a pentagonal fractal in a graphical viewer. By pressing the right mouse button on the window, a pop-up menu with various functions is displayed, the first two functions define the points and the color of the fractal, the third and fourth define whether the fractal will appear as a pentagon or a hexagon , while the last terminates the program. In order for the code of this project to work you have to connect Visual Studio with OpenGL, see the corresponding readme file and the code of this project on github.",
+        Link: "https://github.com/Klajdis32/PentagonalFractalWithC" ,
+        pdf: "",
+        file: "",
+        video: ""
+    },
+    {
         image: "/npw/ProjectImages/Aero.jpg", 
         Titlos: "Simulation of linear motion", 
         Sigrafeas: "klajdi", 
@@ -8,7 +128,8 @@ const posts = [
         Keimeno: "This project delves into the realm of physics, exploring the experimental representation of linear, smooth, and accelerated body motion. Our aim is to corroborate the mathematical equations that describe these motions through real-world experimentation. The process utilizes a unique setup - the layout of an air route, supported by a computer for data recording and analysis. The mathematical validity is ensured by employing the method of Ordinary Least Squares.",
         Link: "" ,
         pdf: "/npw/attachments/pdfs/SimulationOfLinearMotion.pdf",
-        file: ""
+        file: "",
+        video: ""
     },
     {
         image: "/npw/ProjectImages/arduino.jpg", 
@@ -19,6 +140,7 @@ const posts = [
         Keimeno: "In this project I had to connect a potentiometer to a servo and thus determine through it the angle that the servo will move without putting the angle in the c++ code. The led is connected to a 150 Ω resistor and simply when the angles I determine through the potentiometer are small, it flashes very quickly, while when the angle increases, its frequency decreases.",
         Link: "https://www.tinkercad.com/things/kSZySV6dCZB?sharecode=ktuk_1gy83ruaVoYp3rV64uu0xQLY0x-EDiKvdFDQIs",
         pdf: "",
-        file: ""
+        file: "",
+        video: ""
     },
 ];
