eddf2c5a4df6692dfa2d1b2695159cc9ec9d8833
klaintis <kcami32@outlook.com>
2024-03-24 21:34:55 +0200

    books

---
+++ BooksDB.js
@@ -0,0 +1,23 @@
+const books = [
+    {
+        id: "3",
+        Titlos: "Biblio3", 
+        bathmos: "7",
+        simperasma: "simperasma3",
+        sxolio: "sxolio3"
+    },
+    {
+        id: "2",
+        Titlos: "Biblio2", 
+        bathmos: "8",
+        simperasma: "simperasma2 simperasma2 simperasma2 simpsimperasma2dsas simperasma2 simperasma2",
+        sxolio: "sxolio2"
+    },
+    {
+        id: "1",
+        Titlos: "Biblio1", 
+        bathmos: "6",
+        simperasma: "simperasma1",
+        sxolio: "sxolio1"
+    }
+];
